47d18c047760b39ce0a1279a1a5e4588110d4880
ikabadzhov <kabadzhovivan@gmail.com>
2019-11-16 13:37:27 +0100

    Classes v1.1.

--- app/src/main/java/com/example/junctionhack/Events.java
+++ app/src/main/java/com/example/junctionhack/Events.java
@@ -5,14 +5,14 @@ public class Events {
     private String baggageId;
     private String airport; // Airport of the event, three-letter IATA code
     private String timestamp;
-    private enum type {
-        CHECKED_IN,
-        LOADED,
-        UNLOADED,
-        DAMAGED,
-        CLAIMED,
-        MISSING;
-    }
+
+    private boolean CHECKED_IN;
+    private boolean LOADED;
+    private boolean UNLOADED;
+    private boolean DAMAGED;
+    private boolean CLAIMED;
+    private boolean MISSING;
+
 
     public String getEventId(){
         return eventId;
@@ -28,6 +28,30 @@ public class Events {
 
     public String getTimestamp(){
         return timestamp;
+    }
+
+    public boolean stateCHECKED_IN(){
+        return CHECKED_IN;
+    }
+
+    public boolean stateLOADED(){
+        return LOADED;
+    }
+
+    public boolean stateUNLOADED(){
+        return UNLOADED;
+    }
+
+    public boolean stateDAMAGED(){
+        return DAMAGED;
+    }
+
+    public boolean stateCLAIMED(){
+        return CLAIMED;
+    }
+
+    public boolean stateMISSING(){
+        return MISSING;
     }
 
 }
